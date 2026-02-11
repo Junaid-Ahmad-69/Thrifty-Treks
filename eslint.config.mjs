4e74ec5a558858c20fbdf0e844fde1cfff06b35d
junaid-ahmad <junaidsandhu21@gmail.com>
2026-02-11 17:22:44 +0500

    add husky configurations, added github actions ci/cd pipeline

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import { antfu } from "@antfu/eslint-config";
+import nodePlugin from "eslint-plugin-n";
+
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(
+  antfu({
+    type: "app",
+    vue: true,
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: "double",
+    },
+    ignores: [".pnpm-store/**", "**/migrations/*"],
+  }, {
+    plugins: {
+      n: nodePlugin,
+    },
+    rules: {
+      "vue/max-attributes-per-line": ["error", {
+        singleline: { max: 2 },
+        multiline: { max: 1 },
+      }],
+      "ts/no-redeclare": "off",
+      "ts/consistent-type-definitions": ["error", "type"],
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "n/prefer-global/process": ["off"],
+      "n/no-process-env": ["error"],
+      "perfectionist/sort-imports": ["error", {
+        type: "alphabetical",
+        order: "asc",
+        ignoreCase: true,
+      }],
+      "unicorn/filename-case": ["error", {
+        case: "kebabCase",
+        ignore: ["README.md"],
+      }],
+    },
+  }),
+);
